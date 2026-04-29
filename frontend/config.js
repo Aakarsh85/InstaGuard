575a711c3c0fba1599ee1b8a6da4e71019e4bd9c
Aakarsh Kumar <aakarshvaidwan@gmail.com>
2026-04-30 02:46:13 +0530

    Revert "splited into smaller parts"
    This reverts commit 2f9fdb0f458b66f8d110a1114fe0dadb09dc8264.

--- frontend/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const CONFIG = {
-  API_BASE: window.location.hostname === "localhost"
-    ? "http://localhost:10000"
-    : "https://instaguard-backend-2ldg.onrender.com",
-  PAGE_SIZE: 10,
-};
